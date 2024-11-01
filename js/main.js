7d90d15cdb8b9651c4515716c5f032d85768c447
Toxirbek2000 <toxirbekaliqulov2000@gmail.com>
2024-11-01 20:43:59 +0500

    Exerciese end

---
+++ js/main.js
@@ -0,0 +1,116 @@
+
+
+// Masala N1
+
+// for (let i = 1; i <= 20; i++ ){
+//     console.log(i);
+// }
+
+// Masala N2
+
+// for (let i = 1; i <= 66; i++ ){
+//     if(i % 15 == 0){
+//         console.log(i);
+//     }
+// }
+
+// Masala N3
+
+// for (let i = 17; i >= 1; i--){
+//     console.log(i);
+// }
+
+// Masala N4
+
+// let sum = 0
+// let count = 0
+// for(let a = 1; a <= 7; a++){
+//     sum = sum + a
+//     count += 1
+// }
+// console.log(sum);
+// console.log(count);
+
+// Masala N5
+
+// for(let i = 1; i <= 15; i++){
+//     if (15 % i == 0)
+//         console.log(i);   
+// }
+
+// Masala N6
+
+// let sum = 0
+// for(let i = 1; i <= 20; i++){
+//     if (20 % i == 0)
+//         sum = sum + i   
+// }
+// console.log(sum);
+
+
+// Masala N7
+
+// let sum = 1
+// for(let a = 5; a <= 49; a++){
+//     if(a % 2 !== 0){
+//         sum *= a
+//     }
+// }
+// console.log(sum);
+
+
+// while o'tmaganligimiz sababli, Ustozimiz, while bor masalalarni oddiy ishlab qo'yaveringlar dedi
+
+// Masala N8 
+
+// let count = 0
+// for(let i = 4; i <= 29; i++){
+//     if(28 % i == 0)
+//       count = count + 1
+// }
+// console.log(count + " martta joylashtirish mumkin");
+
+
+// Masala N9  
+
+// let A = 15
+// let B = 2
+// let qoldiq = A % B
+
+// console.log((qoldiq));
+
+
+// Masala N10
+
+// let n = prompt("Mahsulotni vaznini kiriting")
+// let pricePerKg = 2500
+// for(let i = 0.1; i <= n; i += 0.1 ){
+//     let price = n * pricePerKg
+//     console.log(Number(i.toFixed(n)) * pricePerKg + " so'm");
+// }
+
+
+// Masala N11
+
+    // let number = prompt("Ikki honali son kiriting")
+    // let tens = Math.floor(number / 10);            // O'nliklar xonasi
+    // let units = number % 10;           // Birliklar xonasi
+
+    // if (tens < units) {
+    //     for (let i = tens; i <= units; i++) {
+    //         console.log(i);
+    //     }
+    // } else {
+    //     for (let i = units; i <= tens; i++) {
+    //         console.log(i);
+    //     }
+    // }
+
+
+
+// Masala N12
+
+// 12-masala tushunarsiz ekan yoki men tushunmadim
+
+
+
